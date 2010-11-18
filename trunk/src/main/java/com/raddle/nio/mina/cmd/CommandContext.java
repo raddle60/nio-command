01c6e39dad70ff5ae9b6c78284258c2a992b6196
raddle60 <raddle60@6ad04df4-bd3a-ffd4-ca7e-2509e9116f6f>
2010-11-18 07:40:32 +0000

    在需要时创建sender

--- trunk/src/main/java/com/raddle/nio/mina/cmd/CommandContext.java
+++ trunk/src/main/java/com/raddle/nio/mina/cmd/CommandContext.java
@@ -3,6 +3,8 @@
  */
 package com.raddle.nio.mina.cmd;
 
+import org.apache.mina.core.session.IoSession;
+
 import com.raddle.nio.mina.cmd.api.CommandSender;
 
 /**
@@ -11,12 +13,25 @@ import com.raddle.nio.mina.cmd.api.CommandSender;
  */
 public class CommandContext {
 	private static ThreadLocal<CommandSender> commandSender = new ThreadLocal<CommandSender>();
+	private static ThreadLocal<IoSession> ioSession = new ThreadLocal<IoSession>();
 
 	public static CommandSender getCommandSender() {
+		if (commandSender.get() == null && ioSession.get() != null) {
+			commandSender.set(new SessionCommandSender(ioSession.get()));
+		}
 		return commandSender.get();
 	}
 
-	public static void setCommandSender(CommandSender commandSender) {
-		CommandContext.commandSender.set(commandSender);
+	public static IoSession getIoSession() {
+		return ioSession.get();
+	}
+
+	public static void setIoSession(IoSession ioSession) {
+		CommandContext.ioSession.set(ioSession);
+	}
+
+	public static void clear() {
+		CommandContext.ioSession.set(null);
+		CommandContext.commandSender.set(null);
 	}
 }
