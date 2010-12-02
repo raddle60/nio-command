8ce735f94008f99324c5d2e9bc39b8d3b041b74b
raddle60 <raddle60@6ad04df4-bd3a-ffd4-ca7e-2509e9116f6f>
2010-12-02 10:06:18 +0000

    默认并发执行

--- src/main/java/com/raddle/nio/mina/cmd/handler/AbstractCommandHandler.java
+++ src/main/java/com/raddle/nio/mina/cmd/handler/AbstractCommandHandler.java
@@ -1,5 +1,12 @@
 package com.raddle.nio.mina.cmd.handler;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IoSession;
 import org.slf4j.Logger;
@@ -12,9 +19,32 @@ import com.raddle.nio.mina.exception.ExceptionWrapper;
 
 public abstract class AbstractCommandHandler extends IoHandlerAdapter {
 	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+	protected boolean isConcurrent = true;
+	protected int maxTaskThreads = 10;
+	protected ExecutorService executorService = null;
 
 	@Override
-	public void messageReceived(IoSession session, Object message) throws Exception {
+	public void messageReceived(final IoSession session, final Object message) throws Exception {
+		if(isConcurrent && executorService == null){
+			synchronized (this) {
+				if(executorService == null){
+					executorService = new ThreadPoolExecutor(0, maxTaskThreads, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>() , new DaemonThreadFactory());
+				}
+			}
+		}
+		if(isConcurrent){
+			executorService.execute(new Runnable() {
+				@Override
+				public void run() {
+					processMessage(session, message);
+				}
+			});
+		} else {
+			processMessage(session, message);
+		}
+	}
+
+	private void processMessage(final IoSession session, final Object message) {
 		if (message != null && message instanceof CommandBodyWrapper) {
 			CommandBodyWrapper wrapper = (CommandBodyWrapper) message;
 			Object body = wrapper.getBody();
@@ -52,5 +82,31 @@ public abstract class AbstractCommandHandler extends IoHandlerAdapter {
 	 * @return 处理结果,null不发送数据给client
 	 */
 	protected abstract Object processCommand(Object command) throws Exception;
+	
+    static class DaemonThreadFactory implements ThreadFactory {
+        static final AtomicInteger poolNumber = new AtomicInteger(1);
+        final ThreadGroup group;
+        final AtomicInteger threadNumber = new AtomicInteger(1);
+        final String namePrefix;
+
+        DaemonThreadFactory() {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null)? s.getThreadGroup() :
+                                 Thread.currentThread().getThreadGroup();
+            namePrefix = "daemon-pool-" +
+                          poolNumber.getAndIncrement() +
+                         "-thread-";
+        }
+
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r,
+                                  namePrefix + threadNumber.getAndIncrement(),
+                                  0);
+            t.setDaemon(true);
+            if (t.getPriority() != Thread.NORM_PRIORITY)
+                t.setPriority(Thread.NORM_PRIORITY);
+            return t;
+        }
+    }
 
 }
