a5be12e5045b44dc8db0f88e98faf99ef81a80d8
raddle60 <raddle60@6ad04df4-bd3a-ffd4-ca7e-2509e9116f6f>
2010-12-17 03:03:00 +0000

    limit

--- src/test/java/com/raddle/nio/mina/ServerMain.java
+++ src/test/java/com/raddle/nio/mina/ServerMain.java
@@ -11,10 +11,10 @@ import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 import org.omg.CORBA.BooleanHolder;
 
+import com.raddle.nio.codec.impl.HessianCodec;
 import com.raddle.nio.mina.cmd.SessionCommandSender;
 import com.raddle.nio.mina.cmd.handler.AbstractCommandHandler;
-import com.raddle.nio.mina.hessian.HessianDecoder;
-import com.raddle.nio.mina.hessian.HessianEncoder;
+import com.raddle.nio.mina.codec.ChainCodecFactory;
 
 public class ServerMain {
 
@@ -26,7 +26,9 @@ public class ServerMain {
 		final BooleanHolder bool = new BooleanHolder(false);
 		final IoAcceptor acceptor = new NioSocketAcceptor();
 		acceptor.getSessionConfig().setReaderIdleTime(10);// 10秒沒收到数据就超时
-		acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new HessianEncoder(), new HessianDecoder()));
+		ChainCodecFactory chainCodecFactory = new ChainCodecFactory();
+		chainCodecFactory.addFirst(new HessianCodec());
+		acceptor.getFilterChain().addFirst("codec", new ProtocolCodecFilter(chainCodecFactory));
 		// 处理接收的命令和响应
 		acceptor.setHandler(new AbstractCommandHandler() {
 
